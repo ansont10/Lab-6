6d0d6c7804baa0703e8fb49743a19fb173404cd3
Anson Tran <ansonop@yahoo.com>
2024-10-19 22:50:55 -0400

    Added Menu and Handling

--- Github/Lab6.py
+++ Github/Lab6.py
@@ -1,6 +1,7 @@
 # Anson Tran
+
 def encode():
-    user_input = input()
+    user_input = input("Enter an 8 digit password to encode: ")
     encoded_res = ""
     if len(user_input) == 8 and user_input.isdigit():      
         for char in user_input:
@@ -9,7 +10,23 @@ def encode():
         return encoded_res
     else:
         return "Invalid input, please enter an 8 digit password."
-        
-         
-encoded_password = encode()
-print(encoded_password)
+    print(encoded_res)
+def decode():
+    pass
+
+program_continue = True
+while program_continue:
+    print("\nMenu\n1. Encode\n2. Decode\n3. Exit")
+    try:
+        menu_selection = int(input("Select an option: "))
+        if menu_selection == 1:
+            encoded_password = encode()
+            print(encoded_password)
+        elif menu_selection == 2:
+            pass
+        elif menu_selection == 3:
+            program_continue = False
+        else:
+            print("Invalid selection, please select one of the options")
+    except ValueError:
+        print("Invalid input, please select one of the options.")
